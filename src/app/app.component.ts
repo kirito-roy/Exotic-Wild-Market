a740ca86be64c63753193ae9dba4b7c8f72b767a
sumit <roy184433@gmail.com>
2025-01-01 17:26:57 +0530

    sd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Msg } from './service/msg/msg.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'shopping';
+  msg = 'Are you gay?';
+
+  height!: number;
+  width!: number;
+
+  constructor(private msgService: Msg) {
+    this.height = screen.height;
+    this.width = screen.width;
+  }
+
+  yes() {
+    this.msgService.showWarning('Really!!!!!');
+    this.msg = 'You are Gay';
+  }
+
+  entered() {
+    let no = document.getElementById('no') as HTMLButtonElement;
+    let random_y =
+      Math.floor(
+        Math.random() *
+          (this.height - (this.height / 10) * 3 - this.height / 10)
+      ) +
+      this.height / 10;
+    let random_x =
+      Math.floor(
+        Math.random() * (this.width - (this.width / 10) * 3 - this.width / 10)
+      ) +
+      this.width / 10;
+
+    const noId = no?.id;
+    if (noId) {
+      no.style.position = 'fixed';
+      no.style.top = random_y + 'px';
+      no.style.left = random_x + 'px';
+    } else {
+      console.error('No element with id "no" found');
+    }
+  }
+  beat() {
+    this.msg = 'You Won!!!!!';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+//import all the ngprime modules
+import {TableModule} from 'primeng/table';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import {DialogModule} from 'primeng/dialog';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +30,14 @@ import {ButtonModule} from 'primeng/button';
     HttpClientModule,
     BrowserAnimationsModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    CardModule,
+    TableModule,
+    DropdownModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    ToastModule,
+    DialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
